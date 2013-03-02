c479da6a853631e42eaae84a2d359bce2281c325
Rasmus Scholer <rasmusscholer@gmail.com>
2013-03-02 15:25:36 +0100

    Created rsenv.combinationexclude function which can be used to produce a list or string with combinations of elements, excluding certain pairs/combinations. Can be expressed as a one-liner and easily adjusted as needed.

--- rsenv/rsvarious.py
+++ rsenv/rsvarious.py
@@ -33,6 +33,12 @@ def clipboard():
     print ret
     return ret
 
+def cbget():
+    print "Clipboard content: gtk.clipboard_get().wait_for_text()"
+    return gtk.clipboard_get().wait_for_text()
+
+def cbset(txt):
+    gtk.clipboard_get().set_text(txt)
 
 def wikipagenamefromurl(source=None):
     if source is None:
@@ -45,6 +51,7 @@ def wikipagenamefromurl(source=None):
         return ret
     except IndexError:
         print "IndexError: " + str(source.strip().split("/"))
+
 
 """
 -------------
@@ -86,6 +93,44 @@ def rgbhextodec(color):
   if color[0] == '#':
     color = color[1:]
   return (int(color[0:2], 16), int(color[2:4], 16), int(color[4:6], 16))
+
+
+def combinationsexclude(elems, r=2, excludepairs=set(), doprint=True, returnstr=False, rsdebug=True):
+    """ 
+    Do combinatorics (default=binary) and filter the list to exclude certain pairs.
+    E.g. you want to create all possible combinations of the numbers 1,2,3,4,5,6
+    but you do not want to include combinations of: (1 and 2) or (3 and 5) or (4 and 6).
+    I.e. you want: (1, 3), (1, 4), (1, 5), (1, 6), (2, 3), (2, 4), (2, 5), (2, 6), (3, 4), (3, 6), (4, 5), (5, 6)
+    Use this method like: combinationsexclude(range(1,6), [(1,2),(3,5),(4,6)])
+    Edit: Can now be run with r > 2, e.g. 
+        rsenv.bincombexclude(range(1,5),[(2,1,3)],r=3)
+    returns [(1, 2, 4), (1, 3, 4), (2, 3, 4)]
+    Can easily be run as a copy/paste friendly oneliner as seen. See refs to augment to your needs.
+    Refs:
+     - http://docs.python.org/2/library/itertools.html
+     - http://docs.python.org/2/library/sets.html
+    """
+    if isinstance(elems,int):
+        elems = range(elems)
+    excludepairs = set(excludepairs) # Make sure we have a set of tuples and not just a list of tuples.
+    import itertools
+    def isok(tup):
+        return not (tup in excludepairs or reversed(tup) in excludepairs)
+    #F = [tup for tup in itertools.combinations(range(1,7),2) if isok(tup)]
+    # This is only guaranteed to work if r <= 2.(It might work, not certain)
+    #F = [tup for tup in itertools.combinations(elems,r) if not (tup in excludepairs or reversed(tup) in excludepairs)]
+    # If r > 2, I have to check whether any permutation of the tuple is among the excluded pairs list:
+    H = [tup for tup in itertools.combinations(elems,r) if not set(itertools.permutations(tup)).intersection(excludepairs)] 
+#    set1.intersection(set2) shorthand: set1 & set2
+    # filter-based alternative. I generally find the list-comprehension-with-condition easier to read.
+    #G = filter(lambda tup: not (set(itertools.permutations(tup)).intersection(excludepairs)), itertools.combinations(elems,r))
+    s="\n".join([" ".join([str(i) for i in tup]) for tup in H])
+    if doprint:
+        print s
+    if returnstr:
+        return s
+    return H
+
 
 
 """
